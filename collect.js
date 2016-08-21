7b640173cf956e808daf39a8da040e77cf000d39
sedat sevgili <sedatsevgili@gmail.com>
2016-08-22 02:39:39 +0300

    http collector is built with bl

---
+++ collect.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var bl = require('bl');
+
+http.get(process.argv[2], function(response) {
+	response.setEncoding('utf8');
+	response.pipe(bl(function(err, data) {
+		console.log(data.length);
+		console.log(data.toString());
+	}));
+});
