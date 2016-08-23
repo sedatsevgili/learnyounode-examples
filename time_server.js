e1449064198e053a45e92120ac52470eb9dbb74d
sedat sevgili <sedatsevgili@gmail.com>
2016-08-23 07:44:30 +0300

    tcp time server included but it is not working expectedly

---
+++ time_server.js
@@ -0,0 +1,21 @@
+var net = require('net');
+
+if(process.argv.length > 2) {
+	var server = net.createServer(connectionHandler);
+	server.listen(process.argv[2]);
+}
+
+function connectionHandler(socket) {
+	socket.write(formatDate(new Date()));
+	socket.end();
+}
+
+function formatDate(date) {
+	return 	date.getFullYear() + '-' + zeroish(date.getMonth() + 1) + '-' + zeroish(date.getDate()) + ' ' + zeroish(date.getHours()) + ':' + zeroish(date.getMinutes());
+	
+}
+
+function zeroish(value) {
+	return value < 10 ? ('0' + value) : ('' + value);
+}
+
